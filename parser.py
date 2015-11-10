fad667eb886b77b94a0e0a6fa94057b7599b29fe
Anders Brandt <anderswb@gmail.com>
2015-11-10 21:13:03 +0100

    Started work on aliases

--- parser.py
+++ parser.py
@@ -4,18 +4,45 @@ import sqlite3
 import sys
 import csv
 
+print('Creating fighters dictionary from csv file')
 fighters = dict()
 with open('fighters.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=';')
     for row in readCSV:
-        fighters[row[0]] = {'weightclasses': row[1], 'aliases': row[2]}
+        if row[2]:
+            aliases = row[2].split(',')
+            aliases = [alias.strip() for alias in aliases]
+        else:
+            aliases = None
+        fighters[row[0]] = {'weightclasses': row[1], 'aliases': aliases}
 
+print('Testing for duplicate aliases')
+alias_list = []
+for name in fighters:
+    if fighters[name]['aliases']:
+        for alias in fighters[name]['aliases']:
+            if alias in alias_list:
+                print("Warning: Found duplicate alias {}".format(alias))
+            else:
+                alias_list.append(alias)
+
+print('Connecting to database')
 conn = sqlite3.connect('databases/mma.db')
 
 i = 1
 for name in fighters:
     print('Processing fighter {} of {}'.format(i, len(fighters)))
-    cursor = conn.execute("SELECT selftext FROM posts WHERE idstr LIKE 't1_%' AND selftext LIKE '%{}%'".format(name))
+    query = "SELECT selftext " \
+            "FROM posts " \
+            "WHERE idstr LIKE 't1_%' " \
+            "AND (selftext LIKE '%{}%'".format(name)
+
+    if fighters[name]['aliases']:
+        for alias in fighters[name]['aliases']:
+            query = query + " OR selftext LIKE '%" + alias + "%'"
+    query += ')'
+    print('Query:' + query)
+    cursor = conn.execute(query)
     fighters[name]['nameentries'] = len(cursor.fetchall())
     i += 1
 
